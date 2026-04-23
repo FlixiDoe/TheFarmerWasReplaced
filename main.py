cf92330838a33afe54c8904f2600f53812b666ed
FlixiDoe <felixdoernchen@gmail.com>
2026-04-23 16:39:52 +0200

    cost system works

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 clear()
 change_hat(Hats.Wizard_Hat)
+carrotCosts= [-1, -1]
+pumkinCosts= [-1]
+isTree=False	
+
+def setCosts():
+	setCarrotCosts()
+	setPumkinCosts()
 
 
+def setCarrotCosts():
+	global carrotCosts
+	costs= get_cost(Entities.Carrot)
+	carrotCosts[0]= costs[Items.Hay]
+	carrotCosts[1]= costs[Items.Wood]
+
+def setPumkinCosts():
+	global pumkinCosts
+	costs= get_cost(Entities.Pumpkin)
+
+	pumkinCosts[0]= costs[Items.Carrot]
 isTree=False	
 def carrotRessurces():
-	if (num_items(Items.Hay) >= 32 and num_items(Items.Wood) >= 32):
+	if (num_items(Items.Hay) >= carrotCosts[0] and num_items(Items.Wood) >= carrotCosts[1]):
 		return True
 	else:
 		return False
 
 def pumpkinRessurces():
-	if (num_items(Items.Carrot) >= 1):
+	if (num_items(Items.Carrot) >= pumkinCosts[0]):
 		return True
 	else:
 		return False
@@ -21,7 +39,7 @@ def ensureSoil():
 
 def ensureGround():
 	if (get_ground_type() == Grounds.Soil):
-		untill()
+		till()
 
 def plantGrass():
 	ensureGround()
@@ -40,8 +58,10 @@ def plantPumpkin():
 		plant(Entities.Pumpkin)
 		i = 0
 		while not can_harvest():
-			plant(Entities.Pumpkin)
 			needsWater()
+			plant(Entities.Pumpkin)
+			
+			#needsFetrtilizer()
 	else:
 		plantGrass()
 
@@ -68,16 +88,22 @@ def PumkinPlace(size):
 
 
 def needsWater():
-	if (get_water() < 0.5 and num_items(Items.Water) >= 1):
+	if (get_water() < 1 and num_items(Items.Water) >= 1):
 		use_item(Items.Water)
 
 def skipRows(rows):
 	for i in range(rows):
 		move(East)
 
+def needsFetrtilizer():
+	if (num_items(Items.Fertilizer) >= 1):
+		use_item(Items.Fertilizer)
 
+def plantSunflower():
+	print("TODO")
 ##skipRows(3)
 while True:
+	setCosts()
 	for i in range(get_world_size()):
 		
 		if (can_harvest()):
@@ -87,12 +113,16 @@ while True:
 
 		if (x == 0 or x == 11):
 			plantGrass()
-		elif (x == 1 or x == 10):
+		elif (x == 1 or x == 10 or x== 13 or x==15):
 			TreePflanzen()
-		elif (x == 2 or x == 8 or x == 9):
+		elif (x == 2 or x == 8 or x == 9 or x==12 or x==14 ):
 			plantCarrot()
 		elif (x == 3 or x == 4 or x == 5 or x == 6 or x == 7):
 			PumkinPlace(5)
+	
+	
+		else:
+			plantGrass()
 
 		move(North)
 
