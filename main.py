affdc8f97de0857e2db2823f855b704063e1465e
FlixiDoe <felixdoernchen@gmail.com>
2026-04-23 09:28:59 +0200

    before carrot debug

--- main.py
+++ main.py
@@ -3,42 +3,49 @@ change_hat(Hats.Wizard_Hat)
 
 isTree = False
 
-#pumkinBlock = 0
 def carrotRessurces():
 	if (num_items(Items.Hay) >= 16 and num_items(Items.Wood) >= 16):
-		if(get_ground_type= Grounds.till):
-			return True
-		else:
-			till()
-			return True
+		return True
 	else:
-		if(get_ground_type= Grounds.till):
-			
 		return False
-			
+
 def pumpkinRessurces():
 	if (num_items(Items.Carrot) >= 1):
-		return True 
+		return True
 	else:
 		return False
-				
-def plantWithTill(Entity):
+
+def plantWithTill(entity):
 	if (get_ground_type() != Grounds.Soil):
 		till()
-	plant(Entity)
-				
-def plantWithRessurces(Entity):
-	if (Entity == Entities.Carrot):
-		if (carrotRessurces()):
-			plantWithTill(Entity)
-		else:
-			plant(Entities.Grass)
-			
-	if (Entity == Entities.Pumpkin): 
-		if (pumpkinRessurces()):
-			plantWithTill(Entity)
-		else:
-			plant(Entities.Grass)
+	plant(entity)
+
+def plantGrassWithTill():
+	if (get_ground_type() != Grounds.Soil):
+		till()
+		#
+	plant(Entities.Carrot)
+
+def plantCarrot():
+	if (carrotRessurces()):
+		if (get_ground_type() != Grounds.Soil):
+			till()
+		plant(Entities.Carrot)
+	else:
+		if (get_ground_type() != Grounds.Soil):
+			till()
+			#tmp
+		plant(Entities.Carrot)
+
+def plantPumpkin():
+	if (pumpkinRessurces()):
+		if (get_ground_type() != Grounds.Soil):
+			till()
+		plant(Entities.Pumpkin)
+	else:
+		if (get_ground_type() != Grounds.Soil):
+			till()
+		plant(Entities.Grass)
 
 def TreePflanzen():
 	global isTree
@@ -49,54 +56,35 @@ def TreePflanzen():
 		plant(Entities.Tree)
 		isTree = True
 
-def SunnlowerHarvest():
-	plantWithTill(Entities.Sunflower)
-	measure()
-
 def PumkinPlace():
-		pumkinBlock= get_pos_y()
-		maximum= get_world_size()
-		numrowsgone = maximum%5
-		if ((pumkinBlock%5==0 and pumkinBlock!=0) or pumkinBlock> maximum-numrowsgone):
-				plant(Entities.Grass)
-				
-				#print(pumkinBlock)
+	pumkinBlock = get_pos_y()
+	maximum = get_world_size()
+	numrowsgone = maximum % 5
 
-		else:
-			plantWithRessurces(Entities.Pumpkin)
-			#pumkinBlock = pumkinBlock+1
-			#print(pumkinBlock)
-
-		
-	
+	if ((pumkinBlock % 5 == 0 and pumkinBlock != 0) or pumkinBlock > maximum - numrowsgone):
+		plantGrassWithTill()
+	else:
+		plantPumpkin()
 
 while True:
 	for i in range(get_world_size()):
 		if (get_water() < 0.5 and num_items(Items.Water) >= 1):
-			use_item(Items.Water)   
-		
-		if (can_harvest()):
-			if(get_ground_type() == Entities.Sunflower):
-				print("Sunflower")
-			else:
-				harvest()
-			
-		x = get_pos_x()
-		
-		if (x == 0 or x==11):
-			plant(Entities.Grass)
-		elif (x == 1 or x==10):
-			TreePflanzen()
-		elif (x == 2 or x== 8 or x== 9):
-			plantWithRessurces(Entities.Carrot)
-		elif (x == 4 or x == 5 or x == 6 or x == 3 or x == 7):
-			
-			PumkinPlace()
-			
-		elif (x==12): 	
-				SunnlowerHarvest()
-		move(North) 
-		
-	move(East)
+			use_item(Items.Water)
 
-	#get_ground_type = Grounds.
+		if (can_harvest()):
+			harvest()
+
+		x = get_pos_x()
+
+		if (x == 0 or x == 11):
+			plantGrassWithTill()
+		elif (x == 1 or x == 10):
+			TreePflanzen()
+		elif (x == 2 or x == 8 or x == 9):
+			plantCarrot()
+		elif (x == 3 or x == 4 or x == 5 or x == 6 or x == 7):
+			PumkinPlace()
+
+		move(North)
+
+	move(East)
